394ce428d09081e6797cd19ad4920e1a57e387d8
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-10-11 16:45:59 -0400

    c#

--- main.cs
+++ main.cs
@@ -1,7 +1,7 @@
-// Created by: Timothy Manwell
-// Created on: Oct 2022
+//Created by: Timothy Manwell
+//Created on: Oct 2022
 //
-// This program calculates the area and perimeter of a rectangle
+//This program calculates the area and perimeter of a rectangle
 
 using System;
 
@@ -10,8 +10,8 @@ public class Program
 public static void Main(string[] args)
     {
         Console.WriteLine("The dimensions of a rectangle are: 27cm and 52cm.");
-        Console.WriteLine("\nThe area of the rectangle is: " + (27 * 52) + "cm.");
-        Console.WriteLine("\nThe perimeter of the rectangle is: " + (2*(27 + 52)) + "cm².");
+        Console.WriteLine("\nThe area of the rectangle is: " + (27 * 52) + " cm.");
+        Console.WriteLine("\nThe perimeter of the rectangle is: " + (2*(27 + 52)) + " cm².");
         Console.WriteLine("\nDone.");
     }
 }

8578844386d9988eaf7733536c99d8b65ea4536a
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-10-11 16:33:43 -0400

    removed spaces

--- main.cs
+++ main.cs
@@ -8,10 +8,10 @@ using System;
 public class Program
 {
 public static void Main(string[] args)
-        {
-            Console.WriteLine("The dimensions of a rectangle are: 27cm and 52cm.");
-            Console.WriteLine("\nThe area of the rectangle is: " + (27 * 52) + " cm.");
-            Console.WriteLine("\nThe perimeter of the rectangle is: " + (2*(27 + 52)) + " cm².");
-            Console.WriteLine("\nDone.");
-        }
+    {
+        Console.WriteLine("The dimensions of a rectangle are: 27cm and 52cm.");
+        Console.WriteLine("\nThe area of the rectangle is: " + (27 * 52) + " cm.");
+        Console.WriteLine("\nThe perimeter of the rectangle is: " + (2*(27 + 52)) + " cm².");
+        Console.WriteLine("\nDone.");
+    }
 }

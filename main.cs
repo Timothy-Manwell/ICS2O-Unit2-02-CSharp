9fbf0313a4e10c9a433d5b6a9f9e9db5269e5618
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-10-11 15:48:56 -0400

    deleted space

--- main.cs
+++ main.cs
@@ -13,6 +13,6 @@ public class Program
         Console.WriteLine("The dimensions of a rectangle are: 27cm and 52cm.");
         Console.WriteLine("\nThe area of the rectangle is: " + (27 * 52) + " cm.");
         Console.WriteLine("\nThe perimeter of the rectangle is: " + (2*(27 + 52)) + " cm².");
-        Console.WriteLine("\nDone.");
+        Console.WriteLine("\nDone."); 
     }
 }

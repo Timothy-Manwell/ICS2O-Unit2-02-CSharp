4dcd5bf209247d2b78805c6cbf6a542085a137a4
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-10-11 14:47:40 -0400

    c#

--- main.cs
+++ main.cs
@@ -10,7 +10,7 @@ public class Program
   
     public static void Main(string[] args) 
     {
-        Console.WriteLine("\nThe dimensions of a rectangle are: 27cm and 52cm.");
+        Console.WriteLine("The dimensions of a rectangle are: 27cm and 52cm.");
         Console.WriteLine("\nThe area of the rectangle is: " + (27 * 52) + " cm.");
         Console.WriteLine("\nThe perimeter of the rectangle is: " + (2*(27 + 52)) + " cm².");
         Console.WriteLine("\nDone.");
